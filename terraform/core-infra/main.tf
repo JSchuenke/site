940dd34d69b49523420bbdf739c576a2cd7ff89b
JSchuenke <jakeschuenke@gmail.com>
2024-04-14 16:20:27 -0600

    remove var

--- terraform/core-infra/main.tf
+++ terraform/core-infra/main.tf
@@ -11,8 +11,6 @@ locals {
   vpc_cidr = "10.0.0.0/16"
   azs      = slice(data.aws_availability_zones.available.names, 0, 3)
 
-  container_name = 
-
   tags = {
     Blueprint  = local.name
     GithubRepo = "github.com/aws-ia/ecs-blueprints"
