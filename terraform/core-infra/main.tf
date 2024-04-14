dc9f8d2bfde2d5d8ddcad735dd94fbdde1d599cd
JSchuenke <jakeschuenke@gmail.com>
2024-04-14 15:44:25 -0600

    fix vpc reference

--- terraform/core-infra/main.tf
+++ terraform/core-infra/main.tf
@@ -81,7 +81,7 @@ resource "aws_lb_target_group" "core-api" {
   name     = "core-api-lb-tg"
   port     = 80
   protocol = "HTTP"
-  vpc_id   = aws_vpc.main.id
+  vpc_id   = module.vpc.vpc_id
 }
 
 
